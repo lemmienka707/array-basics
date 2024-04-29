037ea4e3ea63f778c4a71958aeac5e3da2f49a74
Kaylee Lemmien <lemmienka707@northwested.org>
2024-04-29 10:29:03 -0400

    evidence

---
+++ web-project-starter-main/main.js
@@ -0,0 +1,46 @@
+// Kaylee Lemmien
+// 29 April 2024
+// Project
+
+const firstName = 'Kaylee';
+const lastName = 'Lemmien';
+
+console.log(`*** Script written by ${firstName} ${lastName} of Elk Rapids High School ***`);
+console.log('\n');
+
+const countries = ['Brazil', 'Columbia', 'Peru', 'Paraguay'];
+const length = countries.length;
+
+console.log(`My countries array: ${countries}`)
+console.log(`Length of countries array: ${length} elements`)
+console.log('\n')
+
+countries.push('Argentina')
+console.log('Adding Argentina using the push( ) method:')
+console.log(`${countries}`)
+console.log('\n')
+
+countries.pop()
+console.log('Removing an element using the pop( ) method:')
+console.log(`${countries}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
